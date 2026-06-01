834d8a3d83233dc82938678cf95f88b1112f1b2a
Shreya Sharma <shreyasharma.1510001@gmail.com>
2026-06-01 11:25:51 +0530

    added round-robin and IP hashing algorithm along with healthy flag for each server

--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
-import "sync"
+import (
+	"fmt"
+	"hash/fnv"
+	"log"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"sync"
+	"sync/atomic"
+	"time"
+)
 
 type Server struct{
 	address string
 	weight int
 	connections int64
+	healthy int32
 }
 
 type ServerPool struct {
@@ -17,6 +29,7 @@ func (sp *ServerPool) AddServer(address string, weight int){
 		 address: address,
 		 weight: weight,
 		 connections: 0,
+		 healthy: 1,
 	  })
 } 
 
@@ -24,17 +37,21 @@ func (sp * ServerPool) RemoverServer(address string){
       sp.servers.Delete(address)
 }
 
-func (sp *ServerPool) GetServers() []*Server{
-       servers:= make([]*Server,0)
-	   sp.servers.Range(func(key, value interface{}) bool {
-		   servers = append(servers, value.(*Server))
-		   return true
-	   })
-	   return servers
+func (sp *ServerPool) GetServers() []*Server {
+    servers := make([]*Server, 0)
+    sp.servers.Range(func(key, value interface{}) bool {
+        server := value.(*Server)
+        if atomic.LoadInt32(&server.healthy) == 1 {
+            servers = append(servers, server)
+        }
+        return true
+    })
+    return servers
 }
 
 
-func LeastConnections(servers []*Server) *Server {
+
+func LeastConnections(servers []*Server, _ *http.Request) *Server {
 	var bestServer *Server
 	for _, server := range servers {
 		if bestServer == nil || server.connections < bestServer.connections {
@@ -42,9 +59,118 @@ func LeastConnections(servers []*Server) *Server {
 		}
 	}
 	return bestServer
-
 }
 
+var rrCounter uint64
+
+func RoundRobin(servers []*Server, _ *http.Request) *Server {
+	idx := atomic.AddUint64(&rrCounter, 1) - 1
+	return servers[idx%uint64(len(servers))]
+}
+
+func IPHash(servers []*Server, r *http.Request) *Server {
+	ip, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		ip = r.RemoteAddr
+	}
+	h := fnv.New32a()
+	h.Write([]byte(ip))
+	return servers[h.Sum32()%uint32(len(servers))]
+}
+
+func healthCheck(server *Server) bool {
+	u, err := url.Parse("http://" + server.address + "/health")
+	if err != nil {
+		return false
+	}
+
+	client := http.Client{
+		Timeout: 5 * time.Second,
+	}
+
+	resp, err := client.Get(u.String())
+	if err != nil {
+		return false
+	}
+
+	defer resp.Body.Close()
+	return resp.StatusCode == http.StatusOK
+}
+
+
+func runHealthCheck(sp *ServerPool, interval time.Duration) {
+    for {
+        time.Sleep(interval)
+        sp.servers.Range(func(addr, server interface{}) bool {
+            s := server.(*Server)
+            if healthCheck(s) {
+                atomic.StoreInt32(&s.healthy, 1)
+            } else {
+                atomic.StoreInt32(&s.healthy, 0)
+            }
+            return true
+        })
+    }
+}
+
+
+
+type LoadBalancer struct {
+	pool      *ServerPool
+	algorithm func([]*Server, *http.Request) *Server
+	interval  time.Duration
+}
+
+func(lb *LoadBalancer) ServeHTTP(w http.ResponseWriter, r *http.Request){
+	    servers := lb.pool.GetServers()
+		if len(servers) == 0 {
+			http.Error(w, "Service Unavailable", http.StatusServiceUnavailable)
+			return
+		}
+		server := lb.algorithm(servers, r)
+		if server == nil {
+			http.Error(w, "Service Unavailable", http.StatusServiceUnavailable)
+			return
+		}
+
+		atomic.AddInt64(&server.connections, 1)
+		defer atomic.AddInt64(&server.connections, -1)
+
+		proxy := httputil.NewSingleHostReverseProxy(&url.URL{
+			Scheme: "http",
+			Host: server.address,
+		})
+
+		proxy.ServeHTTP(w, r)
+
+
+
+	}
+
+
+
+
+
 func main() {
+
+	pool := &ServerPool{}
+	algorithm := RoundRobin
+	interval := 10 * time.Second
+	
+	pool.AddServer("localhost:8081", 1)
+	pool.AddServer("localhost:8082", 1)
+	pool.AddServer("localhost:8083", 1)
+
+	lb := &LoadBalancer{
+		pool:      pool,
+		algorithm: algorithm,
+		interval:  interval,
+	}
+
+	go runHealthCheck(pool, interval)
+
+	http.Handle("/", lb)
+	fmt.Println("Load Balancer is running on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 
 }
